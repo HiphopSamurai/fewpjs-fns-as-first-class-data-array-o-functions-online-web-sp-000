7f02404ad35d25b057f0f4d318f812846bd9aa9b
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-18 00:29:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function wakeDog(dogName, dogBreed) {
   return "Wake " + dogName + " the " + dogBreed;
 }
+
+function leashDog() {
+  
+}
+
+function walkToPark() {
+  
+}
+
+function throwFrisbee() {
+  
+}
+
+function walkHome() {
+  
+}
+
+function unleashDog() {
+  
+}
